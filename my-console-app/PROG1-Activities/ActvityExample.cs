d2c14c5380723293a32051fcfc602ae99f960349
BenedictGumasing <bengumasing24@gmail.com>
2025-08-09 11:25:57 +0800

    Update ActvityExample.cs

--- my-console-app/PROG1-Activities/ActvityExample.cs
+++ my-console-app/PROG1-Activities/ActvityExample.cs
@@ -1,1 +1,18 @@
+using System;
 
+  namespace ActivityExample
+    (
+          public class Demo1
+          (
+              string name = "Mark";
+              int number = 8;
+              bool isStudent = False;
+              char myChar = 'H';
+              name = "Dela Cruz";
+            
+              long longInteger = number;
+            
+              const string myConst = "Hello";
+            
+          )
+    )
